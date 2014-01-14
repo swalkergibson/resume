ae86c08dc4796c15441d35a65f154551f5852622
swalkergibson <swalkergibson@gmail.com>
2014-01-14 11:12:07 -0700

    Initial commit

---
+++ ShaneGibson.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SanDiego\ParadiseValley\Tempe\Entrepreneur\Freelancer\AgencyDeveloper;
+
+use Language\Server\Scripting\PHP;
+use Language\Server\Scripting\Javascript as NodeJS;
+use Language\Server\Scripting\Python;
+use Language\Client\Scripting\Javascript as JS;
+use Language\Markup\HTML;
+use Language\Markup\CSS;
+
+use Framework\PHP\CakePHP;
+use Framework\PHP\CodeIgniter;
+use Framework\PHP\Symfony;
+
+use Framework\JS\Server\Express;
+use Framework\JS\Client\jQuery;
+use Framework\JS\Client\Backbone;
+use Framework\JS\Client\Ember;
+use Framework\JS\Client\Angular;
+
+use Framework\HTML\Bootstrap;
